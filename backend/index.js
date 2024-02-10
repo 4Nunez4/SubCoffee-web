61b013cd1e47fc99d4c31ad3beda35f8132e93bf
4Nune4 <jorgeenrrique123456789@gmail.com>
2024-02-09 22:33:55 -0500

    carpetas Back-end

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,28 @@
-import express from 'express';
+import express from "express";
+import bodyParser from 'body-parser';
+import ejs from "ejs"; 
 
-const app = express()
+const app = express();
+
+
+
+
+
+
+app.use(express.json());
+// app.use(bodyParser.urlencoded({extended:false}));Poder trabajar con el formato json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.set ('view engine','ejs');
+app.set('views','./view');
+
+
+app.get('/documents', (req,res) =>{
+    res.render('documentacion.ejs');
+});
+
 
 app.listen(4000, () => {
-    console.log("Connected on port: ", 4000);
-})
+    console.log("Servidor se esta ejecutando en el puerto 4000");
+});
