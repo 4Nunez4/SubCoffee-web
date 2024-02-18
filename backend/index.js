333e36300a9d7b049e8245815558a94ab1658e3f
katiuskizzz <munozkatalinavalderrama@gmail.com>
2024-02-18 05:14:37 -0500

    postulacion

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,40 @@
 import express from "express";
 import bodyParser from "body-parser";
+import ejs from "ejs";
+import rutaOferta from "./src/routes/router.oferta.js";
+import router from "./src/routes/routes.registro.js";
+import routerDocument from "./src/routes/notificaciones.routes.js";
+import routerChat from "./src/routes/chat.routes.js";
+import rutasSubastas from "./src/routes/subasta.routes.js";
+import rutasSeguimiento from "./src/routes/seguimiento.routes.js";
 import postulacionRoutes from "./src/routes/postulacion.routes.js";
 import variedadesRoute from "./src/routes/variedades.routes.js";
-import ejs from 'ejs' 
 
 const app = express();
 
 
-app.use("/postulacion", postulacionRoutes);
-app.get("/variedad", variedadesRoute);
-
-
-// app.use(express.json());
 app.use(express.json());
 // app.use(bodyParser.urlencoded({extended:false}));Poder trabajar con el formato json
-// app.use(bodyParser.json());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.set ('view engine','ejs');
-app.set('views','./view');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.get('/documents', (req,res) =>{
-    res.render('documentacion.ejs');
+
+app.use("/subasta",rutasSubastas);
+app.use("/segimiento",rutasSeguimiento);
+app.use("/subcoffee", rutaOferta);
+app.use("/usuario", router);
+app.use("/user", routerChat);
+app.use("/user", routerDocument);
+app.use("/postulacion", postulacionRoutes);
+app.use("/variedad", variedadesRoute);
+
+app.set("view engine", "ejs");
+app.set("views", "./view");
+
+app.get("/documents", (req, res) => {
+  res.render("documentacion.ejs");
 });
-
-
 app.listen(4000, () => {
-    console.log("Servidor se esta ejecutando en el puerto 4000");
+  console.log("Servidor se esta ejecutando en el puerto 4000");
 });
