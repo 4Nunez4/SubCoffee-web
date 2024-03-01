d172a35b8715887f9d4e35c1e1f08e1c3f2f3647
4Nune4 <jorgeenrrique123456789@gmail.com>
2024-02-29 19:09:59 -0500

    front carpeta pages y components

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SubCoffee from './SubCoffee'
+import SubCoffee from './pages/SubCoffee'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
