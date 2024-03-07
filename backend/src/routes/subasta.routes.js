ce6b9eba63ed3dc1d7ae9d234fd248c5af3f95a1
james10munoz <ja6947775@gmail.com>
2024-03-06 23:50:39 -0500

    seguridad en rutas y documentacion

--- backend/src/routes/subasta.routes.js
+++ backend/src/routes/subasta.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { listar, registrar, actualizar, buscar, eliminar } from "../controllers/subasta.controllers.js";
 import { validarActualizarSubasta, validarRegistrarSubasta} from "../validations/subasta.validation.js";
+import { validartoken } from "../controllers/autenticacionController.js";
 
 const router = Router()
 
-router.get("/listar", listar)
-router.post("/registrar",validarRegistrarSubasta, registrar)
-router.put("/actualizar/:id",validarActualizarSubasta, actualizar)
-router.get("/buscar/:id", buscar)
-router.delete("/eliminar/:id", eliminar)
+router.get("/listar",validartoken, listar)
+router.post("/registrar",validartoken, validarRegistrarSubasta, registrar)
+router.put("/actualizar/:id",validartoken, validarActualizarSubasta, actualizar)
+router.get("/buscar/:id",validartoken, buscar)
+router.delete("/eliminar/:id",validartoken, eliminar)
 
 export default router
