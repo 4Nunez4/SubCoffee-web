1df35139f47691b57285701a5950a6d929ab930b
katiuskizzz <munozkatalinavalderrama@gmail.com>
2024-03-07 00:02:46 -0500

    Documentacion y validacion Token

--- backend/src/routes/variedades.routes.js
+++ backend/src/routes/variedades.routes.js
@@ -1,18 +1,19 @@
  import{Router} from 'express';
       import { validartoken } from '../controllers/autenticacionController.js';
 
- import{guardarVariedad,actualizarVariedad,listarVariedad,buscarvariedad, deleteVariedad,desactivarVariedad} from '../controllers/variedad.controllers.js';
+ import{guardarVariedad,actualizarVariedad,listarVariedad,buscarvariedad, deleteVariedad,desactivarVariedad, activarVariedad} from '../controllers/variedad.controllers.js';
 import { validarGuardarVariedad, validarActualizarVar } from '../validations/variedad.validation.js';
  const variedadesRoute= Router();
 
 
 
- variedadesRoute.post('/variedad',validartoken,validarGuardarVariedad, guardarVariedad);
+ variedadesRoute.post('/variedad',validartoken,validartoken,validarGuardarVariedad, guardarVariedad);
  variedadesRoute.put('/variedad/:id',validartoken,validarActualizarVar,actualizarVariedad);
- variedadesRoute.get('/variedad',validartoken,listarVariedad);
+ variedadesRoute.get('/variedad',validartoken, listarVariedad);
  variedadesRoute.get('/variedad/:id',validartoken,buscarvariedad);
  variedadesRoute.put('/desactivar/:id',validartoken, desactivarVariedad)
  variedadesRoute.delete('/variedad/:id',validartoken,deleteVariedad)
+ variedadesRoute.put('/activar/:id',validartoken, activarVariedad)
 
 
 
