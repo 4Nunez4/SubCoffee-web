ece2f3799e2e75cc7400b6b60c6321ba2d92088a
katiuskizzz <munozkatalinavalderrama@gmail.com>
2024-03-08 10:15:56 -0500

    dashboard incompleto

--- front-end/src/components/Dashboard.jsx
+++ front-end/src/components/Dashboard.jsx
@@ -1,13 +1,92 @@
-// src/Dashboard.jsx
 import logo from '../assets/cafe-pergamino.dashboard.jpg';
+import cafe from '../assets/cafe-dashboard-2.jpg'
 
 function Dashboard() {
+ 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-    <div className="bg-white rounded-xl mx-auto w-11/12 md:w-3/4 lg:w-4/5 xl:w-11/12 2xl:w-5/6 h-5/6 ">
-      <img className="w-full h-full object-cover rounded-3xl" src={logo} alt="logo" />
+      <div className="flex flex-col items-center justify-center h-screen">
+          <div className="bg-white mx-auto w-11/12 md:w-3/4 lg:w-4/5 xl:w-11/12 2xl:w-5/6 h-5/6 ">
+              <img className="w-full h-full object-cover rounded-t-3xl pb-8 "src={logo}  />
+
+          <div className="flex flex-wrap justify-center items-center my-12 mx-4">
+
+                <div className="w-full md:w-1/2 p-8 ">
+                    <h2 className="text-3xl font-semibold mb-4">Conectando Campesinos con el Mundo</h2>
+                        <p className="mb-4">En SubCoffe, creemos en el valor de conectar directamente a los campesinos colombianos con amantes del café alrededor del mundo. A través de nuestra plataforma de subastas, garantizamos un precio justo para quienes cultivan y un café de calidad excepcional para ti.</p>
+                        
+                        <button  className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Explora las Subastas</button>
+                    
+            </div>
+  
+                 <div className="w-screen md:w-1/2 pl-60">
+                           <img src={cafe} className="rounded-lg shadow-lg" />
+             </div>
+     
+       <div className="w-full md:w-98 pt-28 ">
+               <h2 className="text-xl font-bold mb-4">Prácticas de SubCoffe</h2>
+                  <div className="grid grid-cols-2 gap-4">
+   
+                           <div className="bg-white p-4 rounded ">
+                              <h3 className="text-lg font-semibold mb-2">Transparencia en la cadena de suministro</h3>
+                                 <p>Ofrecemos información detallada sobre el origen y la producción del café para garantizar transparencia y confianza.</p>
+                                 <button  className="border border-cyan-400 hover:bg-gray-200 text-cyan-500 font-semibold py-1 px-1 rounded m-5 ">Explora las Subastas</button>
+                           </div>
+      
+                     <div className="bg-white p-4 rounded ">
+                            <h3 className="text-lg font-semibold mb-2">Educación sobre el café</h3>
+                                 <p>Aprendizaje accesible sobre granos, métodos de cultivo y preparación para mejorar la apreciación del café.</p>
+                                 <button  className="border border-cyan-400 hover:bg-gray-200 text-cyan-500 font-semibold py-1 px-1 rounded m-5 ">Explora las Subastas</button>
+                     </div>
+     
+                     <div className=" bg-white p-4 rounded ">
+                            <h3 className="text-lg font-semibold mb-2">Compromiso comunitario</h3>
+                                 <p>Apoyamos a los productores con precios justos, capacitación y desarrollo para fortalecer la comunidad cafetalera.</p>
+                                 <button  className="border border-cyan-400 hover:bg-gray-200 text-cyan-500 font-semibold py-1 px-1 rounded m-5 ">Explora las Subastas</button>
+                     </div>
+     
+                     <div className="bg-white p-4 rounded ">
+                           <h3 className="text-lg font-semibold mb-2">Experiencia del usuario centrada en el cliente</h3>
+                                 <p>Diseño intuitivo, opciones claras y proceso de compra seguro para una experiencia de usuario excepcional.</p>
+                                 <button  className="border border-cyan-400 hover:bg-gray-200 text-cyan-500 font-semibold py-1 px-1 rounded m-5 ">Explora las Subastas</button>
+                     </div>
+                 </div>
+        </div>
+
+
+        <div className="w-full md:w-48 pt-28 bg-black  ">
+          <h2 className='text-xl font-bold justify-center '>Subasta la variedad de Café de tu gusto</h2>
+            <div className='grid grid-cols-2 gap-4'>
+
+              
+            <div className="bg-white p-4 rounded ">
+                              <h3 className="text-lg font-semibold mb-2">Transparencia en la cadena de suministro</h3>
+                                 <img src="" alt="" />
+                                 
+                           </div>
+            <div className="bg-white p-4 rounded ">
+               <h3 className="text-lg font-semibold mb-2">Transparencia en la cadena de suministro</h3>
+                  <img src="" alt="" />
+                  
+            </div>
+            <div className="bg-white p-4 rounded ">
+               <h3 className="text-lg font-semibold mb-2">Transparencia en la cadena de suministro</h3>
+                  <img src="" alt="" />
+                  
+            </div>
+            <div className="bg-white p-4 rounded ">
+               <h3 className="text-lg font-semibold mb-2">Transparencia en la cadena de suministro</h3>
+                  <img src="" alt="" />
+                  
+            </div>
+              
+            </div>
+
+        </div>
     </div>
   </div>
+</div>
+
+
   
 
 
--- front-end/src/components/TerminosCondiciones.jsx
+++ front-end/src/components/TerminosCondiciones.jsx
@@ -1,66 +1,50 @@
 export const TerminosyCondiciones = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center w-full">
-            <div className="bg-white dark:bg-white-600 shadow-md rounded-lg px-8 py-6 max-w-md border border-gray-700">
-            <h1 className="">Terminos y Condiciones</h1>
-            <label htmlFor="">Términos y Condiciones de Uso para SubCoffe
+    <div className="min-h-screen flex items-center justify-center w-full p-4">
+      <div className="bg-white bg-white-900 shadow-md rounded-lg px-8 py-6 max-w-4xl border border-gray-700 overflow-y-auto">
+        <h1 className="text-2xl font-bold text-center mb-4">Términos y Condiciones</h1>
+        <p className="mb-4"><strong>Fecha de efectividad:</strong> 2024</p>
+        <p className="mb-4">Bienvenido a SubCoffe, la plataforma de subasta en línea enfocada en el café pergamino de calidad directamente de los campesinos del Huila, Colombia. Al utilizar nuestro sitio web ubicado en [Ingresar URL del Sitio Web] (en adelante, el "Sitio Web"), usted acepta estar vinculado por estos Términos y Condiciones de Uso (en adelante, los "Términos"), que rigen su acceso y uso del Sitio Web y de los servicios ofrecidos por SubCoffe (en adelante, los "Servicios").</p>
+        
+        <p className="mb-4">Por favor, lea estos Términos cuidadosamente antes de acceder o utilizar nuestro Sitio Web. Si no está de acuerdo con todos los términos y condiciones de este acuerdo, entonces no puede acceder al Sitio Web ni utilizar ninguno de los servicios. Al acceder o usar cualquier parte del sitio, usted acepta estar sujeto a estos Términos.</p>
+        
+        <h2 className="text-xl font-semibold mb-3">1. Descripción del Servicio</h2>
+        <p className="mb-4">SubCoffe proporciona una plataforma de subasta en línea que permite a los campesinos del Huila, Colombia, subastar su café pergamino de calidad. Los usuarios registrados pueden participar en las subastas para adquirir café directamente de los productores. SubCoffe actúa como intermediario entre los vendedores (campesinos) y los compradores, pero no participa en el proceso de pago entre las partes.</p>
+        
+        <h2 className="text-xl font-semibold mb-3">2. Registro de Usuarios</h2>
+        <p className="mb-4">Para acceder a ciertas funciones del Sitio Web, incluyendo la participación en subastas, deberá registrarse y crear una cuenta de usuario. Al registrarse, se compromete a proporcionar información verdadera, precisa, actualizada y completa sobre usted, según lo solicitado por el formulario de registro. SubCoffe se reserva el derecho de suspender o terminar su cuenta si se descubre que cualquier información proporcionada es falsa, inexacta, desactualizada o incompleta.</p>
 
-            Fecha de efectividad: [Ingresar Fecha]
+        <h2 className="text-xl font-semibold mb-3">3. Privacidad y Protección de Datos</h2>
+        <p className="mb-4">SubCoffe toma seriamente la privacidad de sus usuarios. Recopilamos y utilizamos sus datos personales de acuerdo con nuestra Política de Privacidad, la cual se encuentra disponible en [Ingresar URL de la Política de Privacidad]. Al utilizar nuestros Servicios, usted consiente la recopilación, uso y compartición de sus datos personales según lo establecido en nuestra Política de Privacidad.</p>
 
-            Bienvenido a SubCoffe, la plataforma de subasta en línea enfocada en el café pergamino de calidad directamente de los campesinos del Huila, Colombia. Al utilizar nuestro sitio web ubicado en [Ingresar URL del Sitio Web] (en adelante, el "Sitio Web"), usted acepta estar vinculado por estos Términos y Condiciones de Uso (en adelante, los "Términos"), que rigen su acceso y uso del Sitio Web y de los servicios ofrecidos por SubCoffe (en adelante, los "Servicios").
+        <h2 className="text-xl font-semibold mb-3">4. Cookies y Tecnologías de Seguimiento</h2>
+        <p className="mb-4">El Sitio Web utiliza cookies y otras tecnologías de seguimiento para mejorar su experiencia de usuario y recopilar datos sobre cómo utiliza el sitio. Al usar nuestro Sitio Web, usted acepta el uso de estas tecnologías, según se describe en nuestra Política de Privacidad.</p>
 
-            Por favor, lea estos Términos cuidadosamente antes de acceder o utilizar nuestro Sitio Web. Si no está de acuerdo con todos los términos y condiciones de este acuerdo, entonces no puede acceder al Sitio Web ni utilizar ninguno de los servicios. Al acceder o usar cualquier parte del sitio, usted acepta estar sujeto a estos Términos.
+        <h2 className="text-xl font-semibold mb-3">5. Uso de los Servicios</h2>
+        <p className="mb-6 font-medium">
+        Al utilizar los Servicios de SubCoffe, usted se compromete a:
+        </p>
+        <p className="mb-2">-No utilizar los Servicios para fines ilegales o no autorizados.</p>
+        <p className="mb-2">-No recopilar información de otros usuarios sin su consentimiento.</p>
+        <p className="mb-2">-Mantener la confidencialidad de su contraseña y otros datos de acceso a su cuenta.</p>
+        <p className="mb-2">-No publicar contenido falso, inexacto, engañoso, difamatorio o de naturaleza ofensiva.</p>
+        <p className="mb-2">-Cumplir con todas las leyes locales, nacionales e internacionales aplicables.</p>
+        
+        <h2 className="text-xl font-semibold mb-3">6. Derechos de Propiedad Intelectual</h2>
+        <p className="mb-6 ">Todo el contenido incluido en el Sitio Web, como textos, gráficos, logos, imágenes, así como la compilación de dicho contenido, es propiedad de SubCoffe o de sus licenciantes y está protegido por las leyes de derechos de autor de Colombia y leyes internacionales de derechos de autor. Usted se compromete a no reproducir, duplicar, copiar, vender, revender o explotar cualquier parte del Servicio, uso del Servicio, o acceso al Servicio sin el permiso expreso por escrito de SubCoffe.</p>
 
-1. Descripción del Servicio
+        <h2 className="text-xl font-semibold mb-3">7. Modificación de los Términos</h2>
+        <p className="mb-4">SubCoffe se reserva el derecho, a su sola discreción, de modificar o reemplazar estos Términos en cualquier momento. Si las revisiones son significativas, haremos lo posible por notificarle al menos 30 días antes de que los nuevos términos entren en vigencia. Lo que constituye un cambio significativo será determinado a nuestra discreción.</p>
 
-SubCoffe proporciona una plataforma de subasta en línea que permite a los campesinos del Huila, Colombia, subastar su café pergamino de calidad. Los usuarios registrados pueden participar en las subastas para adquirir café directamente de los productores. SubCoffe actúa como intermediario entre los vendedores (campesinos) y los compradores, pero no participa en el proceso de pago entre las partes.
+        <h2 className="text-xl font-semibold mb-3">8. Limitación de Responsabilidad</h2>
+        <p className="mb-4">SubCoffe no será responsable por daños indirectos, incidentales, especiales, consecuenciales o ejemplares, incluidos, pero no limitados a, daños por pérdida de beneficios, buena voluntad, uso, datos u otras pérdidas intangibles, resultantes de su acceso o uso o la imposibilidad de acceder o usar el Servicio.</p>
 
-2. Registro de Usuarios
-
-Para acceder a ciertas funciones del Sitio Web, incluyendo la participación en subastas, deberá registrarse y crear una cuenta de usuario. Al registrarse, se compromete a proporcionar información verdadera, precisa, actualizada y completa sobre usted, según lo solicitado por el formulario de registro. SubCoffe se reserva el derecho de suspender o terminar su cuenta si se descubre que cualquier información proporcionada es falsa, inexacta, desactualizada o incompleta.
-
-3. Privacidad y Protección de Datos
-
-SubCoffe toma seriamente la privacidad de sus usuarios. Recopilamos y utilizamos sus datos personales de acuerdo con nuestra Política de Privacidad, la cual se encuentra disponible en [Ingresar URL de la Política de Privacidad]. Al utilizar nuestros Servicios, usted consiente la recopilación, uso y compartición de sus datos personales según lo establecido en nuestra Política de Privacidad.
-
-4. Cookies y Tecnologías de Seguimiento
-
-El Sitio Web utiliza cookies y otras tecnologías de seguimiento para mejorar su experiencia de usuario y recopilar datos sobre cómo utiliza el sitio. Al usar nuestro Sitio Web, usted acepta el uso de estas tecnologías, según se describe en nuestra Política de Privacidad.
-
-5. Uso de los Servicios
-
-Al utilizar los Servicios de SubCoffe, usted se compromete a:
-
-No utilizar los Servicios para fines ilegales o no autorizados.
-No recopilar información de otros usuarios sin su consentimiento.
-Mantener la confidencialidad de su contraseña y otros datos de acceso a su cuenta.
-No publicar contenido falso, inexacto, engañoso, difamatorio o de naturaleza ofensiva.
-Cumplir con todas las leyes locales, nacionales e internacionales aplicables.
-6. Derechos de Propiedad Intelectual
-
-Todo el contenido incluido en el Sitio Web, como textos, gráficos, logos, imágenes, así como la compilación de dicho contenido, es propiedad de SubCoffe o de sus licenciantes y está protegido por las leyes de derechos de autor de Colombia y leyes internacionales de derechos de autor. Usted se compromete a no reproducir, duplicar, copiar, vender, revender o explotar cualquier parte del Servicio, uso del Servicio, o acceso al Servicio sin el permiso expreso por escrito de SubCoffe.
-
-7. Modificación de los Términos
-
-SubCoffe se reserva el derecho, a su sola discreción, de modificar o reemplazar estos Términos en cualquier momento. Si las revisiones son significativas, haremos lo posible por notificarle al menos 30 días antes de que los nuevos términos entren en vigencia. Lo que constituye un cambio significativo será determinado a nuestra discreción.
-
-8. Limitación de Responsabilidad
-
-SubCoffe no será responsable por daños indirectos, incidentales, especiales, consecuenciales o ejemplares, incluidos, pero no limitados a, daños por pérdida de beneficios, buena voluntad, uso, datos u otras pérdidas intangibles, resultantes de su acceso o uso o la imposibilidad de acceder o usar el Servicio.
-
-9. Jurisdicción y Ley Aplicable
-
-Estos Términos se regirán e interpretarán de acuerdo con las leyes de Colombia, sin dar efecto a ningún principio de conflictos de leyes. Cualquier disputa relacionada con estos Términos o el Servicio será resuelta en los tribunales competentes ubicados en Colombia.
-
-10. Contacto
-
-Si tiene alguna pregunta sobre estos Términos, por favor contáctenos en [Ingresar Información de Contacto].
-
-Al acceder o utilizar nuestro Sitio Web, usted acepta estar sujeto a estos Términos y Condiciones. Gracias por elegir SubCoffe.</label>
-            </div>
+        <h2 className="text-xl font-semibold mb-3">9. Jurisdicción y Ley Aplicable</h2>
+        <p className="mb-4">Estos Términos se regirán e interpretarán de acuerdo con las leyes de Colombia, sin dar efecto a ningún principio de conflictos de leyes. Cualquier disputa relacionada con estos Términos o el Servicio será resuelta en los tribunales competentes ubicados en Colombia.</p>
+        
+        <h2 className="text-xl font-semibold mb-3">10. Contacto</h2>
+        <p>Si tiene alguna pregunta sobre estos Términos, por favor contáctenos en [Ingresar Información de Contacto].</p>
+      </div>
     </div>
-
-  )
-}
-
-
+  );
+};
