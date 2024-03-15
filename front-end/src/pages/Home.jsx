2293ae48008bb9bdbb8628a2e7543ff1fe4e36ec
valen1079534 <valendiazlerma16@gmail.com>
2024-03-14 22:18:56 -0500

    FRONT 45%

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -39,12 +39,14 @@ function Home(){
 
     return(
        
-     <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+     <div className="max-w-[1400px] h-[400px] w-full m-auto py-16 px-4 relative group">
       <div style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className="w-full h-full rounded-2xl bg-center bg-cover duration-500">
       </div>
+
       <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl text-bold rounded-full p-2 bg-black/20 text-white cursor-pointer hidden group-hover:block">
         <FaChevronLeft  onClick={prevSlide} size={33}/>
       </div>
+      
       <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl text-bold rounded-full p-2 bg-black/20 text-white cursor-pointer hidden group-hover:block">
         <FaChevronRight onClick={nextSlide} size={33}t/>
       </div>
@@ -56,62 +58,14 @@ function Home(){
         ))}
       </div>
 
-        <h2>Cafe Robusta Subastas </h2>
+        <h1 className="font-medium">Cafe Robusta Subasta</h1>
+        <div className="max-w-[200px] h-[200px] w-full m-auto py-16 px-4 relative group bg-orange-950  flex-row"></div>
 
-        </div>
+        <h1 className="font-medium">Cafe Arabico Subastas</h1>
+        <div className="max-w-[200px] h-[200px] w-full m-auto py-16 px-4 relative group bg-orange-950"></div></div>
+)}
 
- /* <div className="w-3/4 m-auto">
-            <div className="mt-20">
-                    {data.map((d) => (
+//slidesmini
 
-                        <div className="bg-gray-400 h-[450px] text-black rounded-xl">
-
-                            <div className="rounded-t-xl bg-white flex justify-center items-center bg-indigo-800">
-                                <img src={d.img} alt="" className="h-44 w-44 rounded-full"/>
-                            </div>
-
-                            <div className="flex flex-col justify-center items-center gap-4 p-4">
-                            <p className="text-txl">{d.name}</p>
-                            <p>{d.review}</p>
-                            </div>
-                            <button className=" bg-indigo-500 text-white text-lg px-6 rounded-full">More button</button>
-                        </div>
-                    ))}
-            </div>
-        </div>
-
-const data = [
-    {
-        name: `Cafe robusta`,
-        img:`https://www.semana.com/resizer/LgNm70jTor0z_IKrwZmx8bvlMEY=/arc-anglerfish-arc2-prod-semana/public/MGSRCROCY5GETHHQC2XBMM2CEQ.jpg`,
-        review: `Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsum`
-    },
-
-    {
-        name: `Cafe robusta`,
-        img:`../assets/imagenPrueba.webp`,
-        review: `Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsum`
-    },
-
-    {
-        name: `Cafe robusta`,
-        img:`../assets/imagenPrueba.webp`,
-        review: `Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsum`
-    },
-
-    {
-        name: `Cafe robusta`,
-        img: `../assets/imagenPrueba.webp`,
-        review: `Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsum`
-    },
-
-    {
-        name: `Cafe robusta`,
-        img:`../assets/imagenPrueba.webp`,
-        review: `Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsumipsumLorem ipsum`
-    },
-
-]   */
-    )}
 
 export default Home
