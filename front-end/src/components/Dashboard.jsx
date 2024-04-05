d01cd77f9b9db9fe22fd128b21d5f6b2e4cd6331
katiuskizzz <munozkatalinavalderrama@gmail.com>
2024-04-05 01:01:31 -0500

    Backend funcionando y dashboard

--- front-end/src/components/Dashboard.jsx
+++ front-end/src/components/Dashboard.jsx
@@ -9,14 +9,12 @@ import borbon from '../assets/cafe-borbon.jpg';
 import caturra from '../assets/cafe-caturra.jpg';
 import tabi from '../assets/cafe-tabiejm.jpg';
 import typica from '../assets/cafe-typica.jpg'
+import FootPage from './footPage';
 
 
 function Dashboard() {
-    
-
-    
+ 
   return (
-
       <div className="flex flex-col items-center justify-center min-h-screen">
           <div className=" relative bg-white w-full h-auto overflow-hidden ">
               <img className="w-full h-4/5 object-cover  "src={logo}  />
@@ -70,32 +68,61 @@ function Dashboard() {
           </ul>
           </IconDash>
         </div>
-        {/* <hr className="border-1 border-green-600 w-full mt-24" style={{ borderColor: colors.verdePage }} /> */}
+        <hr className="border-1 border-green-600 w-full mt-24" style={{ borderColor: colors.verdePage }} />
 
+        <h1 className="font-bold pt-12 text-3xl mb-4">Subasta Categorías </h1>
 
+        <div className="w-full pt-12 flex justify-between">
+                    <div className="w-1/4 mx-1 border border-gray-300 rounded-lg p-4">
+                        <p className="text-center font-bold mb-2">Caturra</p>
+                        <img src={caturra} alt="Caturra" className="w-full rounded-lg mb-2" />
+                        <br /><span className="font-semibold text-gray-800">Pitalito, Huila</span> <br /><span className="text-gray-700 pb-6">Fecha de fin: 2024-04-04</span>
+                        <p className="text-sm text-gray-700">La variedad Caturra es conocida por su sabor suave y dulce, con notas de chocolate y frutos secos. </p>
+                    </div>
+                    <div className="w-1/4 mx-1 border border-gray-300 rounded-lg p-4">
+                        <p className="text-center font-bold mb-2">Borbon</p>
+                        <img src={borbon} alt="Borbon" className="w-full rounded-lg mb-2" />
+                        <br /><span className="font-semibold text-gray-800">Pitalito, Huila</span> <br /><span className="text-gray-700 pb-6">Fecha de fin: 2024-04-04</span>
+                        <p className="text-sm text-gray-700">La variedad Borbon tiene un sabor afrutado y floral, con un cuerpo medio y acidez equilibrada.</p>
+                    </div>
+                    <div className="w-1/4 mx-1 border border-gray-300 rounded-lg p-4">
+                        <p className="text-center font-bold mb-2">Tabi</p>
+                        <img src={tabi} alt="Tabi" className="w-full rounded-lg mb-2" />
+                        <br /><span className="font-semibold text-gray-800">Pitalito, Huila</span> <br /><span className="text-gray-700 pb-6">Fecha de fin: 2024-04-04</span>
+                        <p className="text-sm text-gray-700">La variedad Tabi es conocida por su perfil de sabor complejo, con notas frutales y florales y una acidez brillante.</p>
+                    </div>
+                    <div className="w-1/4 mx-1 border border-gray-300 rounded-lg p-4">
+                        <p className="text-center font-bold mb-2">Typica</p>
+                        <img src={typica} alt="Typica" className="w-full rounded-lg mb-2" />
+                        <br /><span className="font-semibold text-gray-800">Pitalito, Huila</span> <br /><span className="text-gray-700 pb-6">Fecha de fin: 2024-04-04</span>
+                        <p className="text-sm text-gray-700">La variedad Typica es apreciada por su sabor suave y equilibrado, con notas cítricas y chocolate.</p>
+                    </div>
+                </div>
+                <hr className="border-1 border-green-600 w-full mt-24" style={{ borderColor: colors.verdePage }} />
+               
+        
+                <div>
+                    <div className="flex justify-center items-center">
+                        <h1 className="text-7xl font-bold pt-10" style={{ color: colors.verdePage }}>Sub</h1>
+                        <h1 className="text-7xl text-black font-sans font-bold pt-10">Coffe</h1>
+                    </div>
+                    <h2 className="text-3xl font-bold mb-4 pt-11">Únete a la Comunidad del Café Especial</h2>
+                </div>
 
-     
-
-        <div>
-  <h1 className="text-4xl md:text-6xl font-bold text-center my-8">Categorías</h1>
-  <div className="flex justify-center items-center space-x-4">
-    <img src={caturra} alt="Imagen 1" className="rounded-lg h-28 md:h-28" />
-    <img src={caturra} alt="Imagen 2" className="rounded-lg h-28 md:h-28" />
-    <img src={caturra} alt="Imagen 3" className="rounded-lg h-28 md:h-28" />
-  </div>
-</div>
-
-</div>
-
-
-
-                
-     
-  </div>
-
-
-
-  );
+                <div className="w-full flex justify-center items-center pt-2">
+                    <div className="w-1/2 mx-32 rounded-lg p-4 text-center">
+                        <p className="text-base text-gray-700 pb-5">Cada clic es una oportunidad. No temas pujar, porque detrás de cada número hay una historia de tierra, sol y dedicación. ¡Haz tu oferta y sé parte de la magia del café!</p>
+                        <ButtonRI to="/registro">Registrarse</ButtonRI>
+                    </div>
+                    <div className="w-1/2 mx-32 rounded-lg p-4 text-center">
+                        <p className="text-base text-gray-700 pb-5">Tu puja es tu voz en esta danza de granos. Eleva tus ofertas con confianza y deja que el aroma del café guíe tus movimientos. ¡Que cada lote sea una sinfonía de pasión y sabor!</p>
+                        <ButtonRI to="/login">Iniciar Sesión</ButtonRI>
+                    </div>
+                </div>
+            </div>
+            <FootPage />
+        </div>
+    );
 }
 
 export default Dashboard;
---
+++ front-end/src/components/footPage.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import logoSub from '../assets/logosubcoffe.png';
+import footPageImg from '../assets/footPageIMG.png';
+import SocialIcons from './atomos/iconFootPge';
+import senaLogo from '../assets/senaLogoPng.png';
+
+function FootPage() {
+    const footerStyle = {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${footPageImg})`, // Superponemos un gradiente negro semitransparente
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '200px', 
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white',
+    };
+
+    const containerStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: '20px', 
+        width: '80%',
+    };
+
+    const logoContainerStyle = {
+        display: 'flex',
+        alignItems: 'center',
+    };
+
+    const logoStyle = {
+        marginLeft: '10px',
+    };
+
+    const socialIconsStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft:'330px', 
+        justifyContent: 'center' 
+    };
+
+    return (
+        <footer style={footerStyle}>
+            <div style={containerStyle}>
+                <div>
+                    <div style={socialIconsStyle}>
+                        <SocialIcons />
+                    </div>
+                    <p>Términos y Condiciones</p>
+                    <p>Soporte de Usuario</p>
+                </div>
+                <div style={logoContainerStyle}>
+                    <img src={senaLogo} alt="SENA Logo" className="w-16 h-16 mx-2" style={logoStyle} />
+                    <img src={logoSub} alt="SubCoffe Logo" className="w-16 h-16 mx-2" style={logoStyle} />
+                </div>
+            </div>
+            <p className="text-sm">© 2024 SubCoffe. Todos los derechos reservados.</p>
+        </footer>
+    );
+}
+
+export default FootPage;
