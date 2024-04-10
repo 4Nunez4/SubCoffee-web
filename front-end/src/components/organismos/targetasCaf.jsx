5487cc85fc292e7c7432dec2941cc7b7a8571daa
4Nune4 <jorgeenrrique123456789@gmail.com>
2024-04-10 17:50:04 -0500

    ajustando targeta de informacion de la finca en subasta

--- front-end/src/components/organismos/targetasCaf.jsx
+++ front-end/src/components/organismos/targetasCaf.jsx
@@ -1,12 +1,47 @@
-import TargetaInfoFinca from "../moleculas/TargetaFinca";
-const TargetasCafetero= ({children})=>{
-    return ( 
-        <div className="h-full w-full">
-            <TargetaInfoFinca/>
-            
-                        {children}
-        </div>
-     );
-}
+import { Card, CardHeader, CardFooter, Image, Button } from "@nextui-org/react";
+const TargetasCafetero = ({ children }) => {
+  return (
+    <div className="  h-full w-full rounded-xl shadow-md overflow-hidden grid grid-cols-4 gap-4 text-cente">
+      <Card
+        isFooterBlurred
+        className="w-full h-[300px] col-span-12 sm:col-span-7"
+      >
+        <CardHeader className="absolute z-10 top-1 flex-col items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">
+            Your day your way
+          </p>
+          <h4 className="text-white/90 font-medium text-xl">
+            Your checklist for better sleep
+          </h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Relaxing app background"
+          className="z-0 w-full h-full object-cover"
+          src="/src/assets/cafe-caturra.jpg"
+        />
+        <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+          <div className="flex flex-grow gap-2 items-center">
+            <Image
+              alt="Breathing app icon"
+              className="rounded-full w-10 h-11 bg-black"
+              src="/images/breathing-app-icon.jpeg"
+            />
+            <div className="flex flex-col">
+              <p className="text-tiny text-white/60">Breathing App</p>
+              <p className="text-tiny text-white/60">
+                Get a good night's sleep.
+              </p>
+            </div>
+          </div>
+          <Button radius="full" size="sm">
+            Get App
+          </Button>
+        </CardFooter>
+      </Card>
+      {children}
+    </div>
+  );
+};
 
 export default TargetasCafetero;
