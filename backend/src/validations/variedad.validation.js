d304628edb7fc0ef50e30a91607116168cf360d4
4Nune4 <jorgeenrrique123456789@gmail.com>
2024-04-16 23:55:11 -0500

    fucion juan

--- backend/src/validations/variedad.validation.js
+++ backend/src/validations/variedad.validation.js
@@ -1,56 +1,5 @@
 import { check } from "express-validator";
 
-<<<<<<< HEAD
-
-export const validarGuardarVariedad = [
-    check("tipo_vari","Por favor seleccione una variedad valida").not().isEmpty().custom((value)=>{
-        const variedades =[ 'Tipica','Borbon','Maragogipe','Tabi','Caturra','Variedad Colombia'];
-        if(!variedades.includes(value)){
-            throw new Error("Tipo invalido");
-        }
-        return true;
-    }),
-
-    check("descripcion_vari","Por favor ingresar la descripcion de la variedad max 250").not().isEmpty().isLength({max:250}),
-    check("puntuacion_vari", "Ingrese la puntuacion de la variedad, solo numeros").not().isEmpty().isInt({ min: 1 }).toInt().isNumeric({ no_symbols: true }).isLength({ max: 15}),
-    check("estado_vari", "El estado es obligatorio por favor ingrese uno de los dos estados permitidos (activo, inactivo)").not().isEmpty().custom((value)=>{
-        const estado =['activo','inactivo'];
-        if(!estado.includes(value)){
-            throw new Error("Estado no valido")
-        }
-        return true;
-    }),
-    check("fk_finca", "Digite un id registrado")
-    .not()
-    .isEmpty()
-    .isInt({ min: 1 })
-    .toInt(),
- 
-]
-export const validarActualizarVar = [
-    check("tipo_vari", "Seleccione una variedad valida").not().isEmpty().custom((value)=>{
-        const variedades =['Tipica','Borbon','Maragogipe','Tabi','Caturra','Variedad Colombia'];
-        if(!variedades.includes(value)){
-            throw new Error("Tipo invalido");
-        }
-        return true;
-    }),
-    check("descripcion_vari", "Por favor ingrese la descripcion de la postulacion").not().isEmpty().isLength({max:250}),
-    check("puntuacion_vari", "Por favor ingrese un valor en la puntuacion").not().isEmpty().isInt({ min: 1 }).toInt().isNumeric({ no_symbols: true }).isLength({ max: 15}),
-    check("estado_vari", "El estado es obligatorio por favor ingrese uno de los dos estados permitidos").not().isEmpty().custom((value)=>{
-        const estado =['activo','inactivo'];
-        if(!estado.includes(value)){
-            throw new Error("Estado no valido")
-        }
-        return true;
-    }),
-    check("fk_finca", "Digite un id registrado")
-    .not()
-    .isEmpty()
-    .isInt({ min: 1 })
-    .toInt(),
-]
-=======
 export const validationVariedad = [
   check("fk_variedad")
     .notEmpty().withMessage("El tipo de variedad es obligatorio")
@@ -68,4 +17,3 @@ export const validationVariedad = [
     .isLength({ max: 11 }).withMessage("La finca de la variedad debe contener menos de 11 caracteres")
     .isInt().isNumeric({ no_symbols:true }),
 ];
->>>>>>> DEVjcrc
