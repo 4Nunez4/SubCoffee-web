b2d88ca7e8546088b92e442e4647cebea0bc4fa9
valendiazlerma@gmail.com <valendiazlerma16@gmail.com>
2024-04-16 20:29:23 -0500

    Back

--- backend/src/controllers/subasta.controllers.js
+++ backend/src/controllers/subasta.controllers.js
@@ -1,21 +1,38 @@
 import { pool } from "../databases/conexion.js";
 import { validationResult } from "express-validator";
 import multer from "multer";
+import path from 'path'
 
 //INSERTAR IMAGEN
 const storage = multer.diskStorage(
     {
-        destination: function(req,img,cb){
+        destination: function(req,file,cb){
             cb(null, "public/img")
         },
 
-        filename: function(req,img,cb){
-            cb(null,img.originalname)
+        filename: function(req,file,cb){
+            cb(null, `${Date.now()}-${file.originalname}`)
         }
     }
 )
-const upload = multer({storage:storage})
-export const cargarImagen= upload.single('img')
+const fileFilter = function(req, file, cb) {
+    const allowedExtensions = /\.(jpg|jpeg|png|pdf)$/i;
+    const extname = path.extname(file.originalname);
+    if (!allowedExtensions.test(extname)) {
+        return cb(new Error('Solo se permiten archivos de imagen (jpg, jpeg, png) o archivos PDF'), false);
+    }
+    cb(null, true);
+};
+
+const upload = multer({ 
+    storage: storage,
+    fileFilter: fileFilter
+});
+
+export const cargarImagen = upload.fields([
+    { name: 'imagen_sub', maxCount: 1 },
+    { name: 'certificado_sub', maxCount: 1 }
+]);
 
 //PDF CARGAR
 /* const storagePdf = multer.diskStorage(
@@ -62,12 +79,13 @@ export const registrarSubasta = async (req, res) => {
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        const  {pk_id_sub, fecha_inicio_sub, fecha_fin_sub, precio_inicial_sub, precio_final_sub, unidad_peso_sub, cantidad_sub, estado_sub, certificado_sub, fk_variedad } = req.body;
+        const  {pk_id_sub, fecha_inicio_sub, fecha_fin_sub, precio_inicial_sub, precio_final_sub, unidad_peso_sub, cantidad_sub, estado_sub, descripcion_sub, fk_variedad } = req.body;
 
-        let imagen_sub = req.file.originalname
+        let imagen_sub = req.files['imagen_sub'] ? req.files['imagen_sub'][0].originalname : null;
+        let certificado_sub = req.files['certificado_sub'] ? req.files['certificado_sub'][0].originalname : null;
         /* let certificado_sub = req.file.originalname */
 
-        const [resultado] = await pool.query("INSERT INTO subasta (pk_id_sub, fecha_inicio_sub, fecha_fin_sub, imagen_sub, precio_inicial_sub, precio_final_sub, unidad_peso_sub, cantidad_sub, estado_sub, certificado_sub, fk_variedad) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [pk_id_sub, fecha_inicio_sub, fecha_fin_sub, imagen_sub, precio_inicial_sub, precio_final_sub, unidad_peso_sub, cantidad_sub, estado_sub, certificado_sub, fk_variedad]);
+        const [resultado] = await pool.query("INSERT INTO subasta (pk_id_sub, fecha_inicio_sub, fecha_fin_sub, imagen_sub, precio_inicial_sub, precio_final_sub, unidad_peso_sub, cantidad_sub, estado_sub, certificado_sub, descripcion_sub, fk_variedad) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)", [pk_id_sub, fecha_inicio_sub, fecha_fin_sub, imagen_sub, precio_inicial_sub, precio_final_sub, unidad_peso_sub, cantidad_sub, estado_sub, certificado_sub, descripcion_sub, fk_variedad]);
 
         if (resultado.affectedRows > 0)
         {
@@ -120,8 +138,36 @@ export const actualizarSubasta = async (req, res) => {
     }
 }
 
+export const desactivarSubasta = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors);
+        }
 
-//BUSCAR-SUBASTA
+        const id = req.params.id;
+
+        const [subastaExiste] = await pool.query("SELECT estado_sub FROM subasta WHERE pk_id_sub = ?", [id]);
+        
+        if (subastaExiste.length === 0) {
+            return res.status(404).json({ status: 404, message: 'El id de la subasta es incorrecto' });
+        } else if (subastaExiste[0].estado_sub === 'cerrada') {
+            return res.status(400).json({ status: 400, message: 'La subasta ya está inactiva' });
+        }
+
+        const [resultado] = await pool.query("UPDATE subasta SET estado_sub = ? WHERE pk_id_sub = ?", ['cerrada', id]);
+        
+        if (resultado.affectedRows > 0) {
+            res.status(200).json({ status: 200, message: "La subasta ha sido desactivada exitosamente" });
+        } else {
+            res.status(500).json({ status: 500, message: "Error al actualizar la subasta" });
+        }
+    } catch (error) {
+        console.error("Error en el bloque try:", error);
+        res.status(500).json({ status: 500, message: 'Error interno del servidor al desactivar la subasta', error });
+    }
+}
+
 
 export const buscar = async (req, res) => {
     try {
@@ -154,7 +200,6 @@ export const buscar = async (req, res) => {
     }
 }
 
-//ELIMINAR SUBASTA
 export const eliminar = async (req, res) => {
     try {
         const id = req.params.id;
@@ -178,39 +223,3 @@ export const eliminar = async (req, res) => {
         });
     }
 };
-
-//SUBASTA ABIERTA
-export const esperaSubasta = async (req, res)=>{
-    try {
-
-        const {id} = req.params
-    
-        const [rows] = await pool.query('UPDATE subasta SET estado_sub=2 WHERE pk_id_sub = ?',[id])
-    
-        if(rows.affectedRows > 0)
-        res.status(200).json({'status':200, 'message':'SUBASTA EN ESPERA'})
-    
-        else
-        res.status(404).json({'status':404, 'message':'Error, no se pudo activar el usuario'})
-    } catch (error) {
-        res.status(500).json({'status':500, 'message':'ERROR SERVIDOR', error})
-    }
-}
-
-// Cerrar
-export const CerrarSubasta = async (req, res)=>{
-    try {
-
-        const {id} = req.params
-    
-        const [rows] = await pool.query('UPDATE subasta SET estado_sub=3 WHERE pk_id_sub = ?',[id])
-    
-        if(rows.affectedRows > 0)
-        res.status(200).json({'status':200, 'message':'SUBASTA CERRADA :)'})
-    
-        else
-        res.status(404).json({'status':404, 'message':'Error, no se pudo activar el usuario'})
-    } catch (error) {
-        res.status(500).json({'status':500, 'message':'ERROR SERVIDOR', error})
-    }
-}
--- backend/src/validations/subasta.validation.js
+++ backend/src/validations/subasta.validation.js
@@ -19,21 +19,28 @@ export const validarRegistrarSubasta=[
     .isInt(),
 ];
 
+
 export const validarActualizarSubasta=[
-    check('fecha_inicio_sub','El formato de fecha-hora no es correct')
+    check('fecha_inicio_sub','El formato de fecha-hora no es correcto')
     .exists()
     .isISO8601()
     .toDate(),
-
     check('fecha_fin_sub','El formato de fecha-hora no es correcto')
     .exists()
     .isISO8601()
     .toDate(),
-
     check('estado_sub','Escriba un estado permitido: abierta, espera o cerrada')
     .exists()
     .isIn(['abierta', 'espera', 'cerrada']),
     check('precio_inicial_sub','Por favor, digite una cantidad')
     .exists()
     .isInt(),
+    check('precio_final_sub','Por favor, digite una cantidad')
+    .exists()
+    .isInt(),
+    check('fk_id_produccion','Digite un id registrado')
+    .not()
+    .isEmpty()
+    .isInt({ min: 1 })
+    .toInt(),
 ]
