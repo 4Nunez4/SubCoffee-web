91d040638842657d2f98154f6bd309475fd153de
4Nune4 <jorgeenrrique123456789@gmail.com>
2024-04-19 16:20:32 -0500

    ajustando validaciones subasta

--- backend/src/routes/subasta.routes.js
+++ backend/src/routes/subasta.routes.js
@@ -1,19 +1,45 @@
 import { Router } from "express";
-import { SubastaAbierta,SubastaCerrada,SubastaEspera,SubastaProceso,subastaFiles, listar, registrar, actualizar, buscar, eliminar } from "../controllers/subasta.controllers.js";
-import { validarActualizarSubasta, validarRegistrarSubasta} from "../validations/subasta.validation.js";
+import {
+  SubastaAbierta,
+  SubastaCerrada,
+  SubastaEspera,
+  SubastaProceso,
+  subastaFiles,
+  listar,
+  registrar,
+  actualizar,
+  buscar,
+  eliminar,
+} from "../controllers/subasta.controllers.js";
+import {
+  validarActualizarSubasta,
+  validarRegistrarSubasta,
+} from "../validations/subasta.validation.js";
 import { verificarUserToken } from "../controllers/autenticacionController.js";
 
-const router = Router()
+const router = Router();
 
-router.post("/registrar",subastaFiles,verificarUserToken, validarRegistrarSubasta, registrar)
-router.get("/listar",verificarUserToken, listar)
-router.put("/actualizar/:id",subastaFiles,verificarUserToken, validarActualizarSubasta, actualizar)
-router.get("/buscar/:id",verificarUserToken, buscar)
-router.delete("/eliminar/:id",verificarUserToken, eliminar)
+router.post(
+  "/registrar",
+  subastaFiles,
+  verificarUserToken,
+  validarRegistrarSubasta,
+  registrar
+);
+router.get("/listar", verificarUserToken, listar);
+router.put(
+  "/actualizar/:id",
+  subastaFiles,
+  verificarUserToken,
+  validarActualizarSubasta,
+  actualizar
+);
+router.get("/buscar/:id", verificarUserToken, buscar);
+router.delete("/eliminar/:id", verificarUserToken, eliminar);
 
-router.delete("/abierta/:id",verificarUserToken, SubastaAbierta)
-router.delete("/cerrada/:id",verificarUserToken, SubastaCerrada)
-router.delete("/espera/:id",verificarUserToken, SubastaEspera)
-router.delete("/proceso/:id",verificarUserToken, SubastaProceso)
+router.put("/abierta/:id", verificarUserToken, SubastaAbierta);
+router.put("/cerrada/:id", verificarUserToken, SubastaCerrada);
+router.put("/espera/:id", verificarUserToken, SubastaEspera);
+router.put("/proceso/:id", verificarUserToken, SubastaProceso);
 
-export default router
+export default router;
