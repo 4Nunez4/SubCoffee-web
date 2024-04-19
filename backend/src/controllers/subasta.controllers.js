92956d529582f3e138bf688fa87ca6b20b1f850a
4Nune4 <jorgeenrrique123456789@gmail.com>
2024-04-19 14:22:23 -0500

    controlador de subasta actualizado ya funciona eliminar buscar y actualizar correctamente

--- backend/src/controllers/subasta.controllers.js
+++ backend/src/controllers/subasta.controllers.js
@@ -1,152 +1,217 @@
+import multer from 'multer';
 import { pool } from "../databases/conexion.js";
 import { validationResult } from "express-validator";
 
-export const listar = async(req, res) => {
-    try {
-        const [ resultado ] = await pool.query("select * from subasta")
+const storage = multer.diskStorage({
+ destination: function (req, file, cb) {
+    cb(null, "public/img/subasta");
+ },
+ filename: function (req, file, cb) {
+    cb(null, file.originalname);
+ },
+});
 
-        if(resultado.length > 0) {
-            res.status(200).json(resultado)
-        }else{
-            res.status(404).json({
-                "mensaje": "No se encontraron subastas :c"
-            })
-        }
+const upload = multer({ storage: storage });
+export const subastaFiles = upload.fields([{ name: 'imagen_sub' }, { name: 'certificado_sub' }]);
 
-    } catch (error) {
-        console.error("Error en el bloque try:", error);
-        res.status(500).json({
-            "mensaje": "Error interno del servidor"
-        });
-    }
-}
-
+// Función para registrar una subasta
 export const registrar = async (req, res) => {
-    try {
-        const errors = validationResult(req);
-       
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        const  { fecha_inicio_sub, fecha_fin_sub, precio_inicial_sub, precio_final_sub, estado_sub, fk_id_produccion } = req.body;        ;
+ try {
+    const errors = validationResult(req);
 
-        // if (!fecha_inicio_sub.trim() || !fecha_fin_sub.trim() || !precio_inicial_sub || !precio_final_sub || !estado_sub.trim() || !fk_id_produccion || !) {
-        //     return res.status(400).json({
-        //         "mensaje": "Por favor, proporcione todos los campos necesarios."
-        //     });
-        // }
-        
-
-        const [resultado] = await pool.query("insert into subasta (fecha_inicio_sub, fecha_fin_sub, precio_inicial_sub, precio_final_sub, estado_sub, fk_id_produccion) values (?, ?, ?, ?, ?, ?)",
-            [fecha_inicio_sub, fecha_fin_sub, precio_inicial_sub, precio_final_sub, estado_sub, fk_id_produccion]);
-
-        if (resultado.affectedRows > 0) {
-            res.status(200).json({
-                "mensaje": "Su subasta ha sido exitosa"
-            });
-        } 
-        
-
-    } catch (error) {
-        console.error("Error en el bloque try:", error);
-        res.status(500).json({
-            "mensaje": "Error interno del servidor"
-        });
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-}
+
+    const {
+      fecha_inicio_sub,
+      fecha_fin_sub,
+      precio_inicial_sub,
+      precio_final_sub,
+      unidad_peso_sub,
+      cantidad_sub,
+      estado_sub,
+      descripcion_sub,
+      fk_variedad,
+    } = req.body;
+
+    let imagen_sub = req.files && req.files['imagen_sub'] ? req.files['imagen_sub'][0].originalname : null;
+    let certificado_sub = req.files && req.files['certificado_sub'] ? req.files['certificado_sub'][0].originalname : null;
+
+    const [resultado] = await pool.query(
+      "INSERT INTO subasta (fecha_inicio_sub, fecha_fin_sub, imagen_sub, precio_inicial_sub, precio_final_sub, unidad_peso_sub, cantidad_sub, estado_sub, certificado_sub, descripcion_sub, fk_variedad) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        fecha_inicio_sub,
+        fecha_fin_sub,
+        imagen_sub,
+        precio_inicial_sub,
+        precio_final_sub,
+        unidad_peso_sub,
+        cantidad_sub,
+        estado_sub,
+        certificado_sub,
+        descripcion_sub,
+        fk_variedad,
+      ]
+    );
+
+    if (resultado.affectedRows > 0) {
+      res.status(200).json({
+        mensaje: "Su subasta ha sido creada con exito",
+      });
+    } else {
+      res.status(400).json({
+        mensaje: "Error al insertar la subasta",
+      });
+    }
+ } catch (error) {
+    console.error("Error en el bloque try:", error);
+    res.status(500).json({
+      mensaje: "Error interno del servidor",
+    });
+ }
+};
+
+// lista todas las subastas en progreso  
+export const listar = async (req, res) => {
+  try {
+    const [resultado] = await pool.query("select * from subasta");
+
+    if (resultado.length > 0) {
+      res.status(200).json(resultado);
+    } else {
+      res.status(404).json({
+        mensaje: "No se encontraron subastas :c",
+      });
+    }
+  } catch (error) {
+    console.error("Error en el bloque try:", error);
+    res.status(500).json({
+      mensaje: "Error interno del servidor",
+    });
+  }
+};
+// actualizar una subasta
 
 export const actualizar = async (req, res) => {
     try {
-        const errors = validationResult(req);
-       
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        const { id } = req.params;
-        const { fecha_inicio_sub, fecha_fin_sub, precio_inicial_sub, precio_final_sub, estado_sub } = req.body;
-
-//        if (!fecha_inicio_sub.trim() || !fecha_fin_sub.trim() || !precio_inicial_sub || !precio_final_sub || !estado_sub.trim() || !fk_id_produccion || !) {
-//           return res.status(400).json({
-//               "mensaje": "Por favor, proporcione todos los campos necesarios."
-//           });
-//       }
-
-        const [resultado] = await pool.query("update subasta set fecha_inicio_sub=COALESCE(?, fecha_inicio_sub), fecha_fin_sub=COALESCE(?, fecha_fin_sub), precio_inicial_sub=COALESCE(?, precio_inicial_sub), precio_final_sub=COALESCE(?, precio_final_sub), estado_sub=COALESCE(?, estado_sub) where pk_id_sub=?",
-            [fecha_inicio_sub, fecha_fin_sub, precio_inicial_sub, precio_final_sub, estado_sub, id]);
-
-        if (resultado.affectedRows > 0) {
-            res.status(200).json({
-                "mensaje": "La subasta ha sido actualizada exitosamente"
-            });
-        } else {
-            res.status(404).json({
-                "mensaje": "No se encontró ninguna subasta con el id proporcionado"
-            });
-        }
-        
-
+       const errors = validationResult(req);
+   
+       if (!errors.isEmpty()) {
+         return res.status(400).json({ errors: errors.array() });
+       }
+   
+       const { id } = req.params;
+       const {
+         fecha_inicio_sub,
+         fecha_fin_sub,
+         precio_inicial_sub,
+         precio_final_sub,
+         unidad_peso_sub,
+         cantidad_sub,
+         estado_sub,
+         descripcion_sub,
+         fk_variedad,
+       } = req.body;
+   
+       // Verificar si los archivos se subieron correctamente
+       let imagen_sub = req.files && req.files['imagen_sub'] && req.files['imagen_sub'][0] ? req.files['imagen_sub'][0].originalname : null;
+       let certificado_sub = req.files && req.files['certificado_sub'] && req.files['certificado_sub'][0] ? req.files['certificado_sub'][0].originalname : null;
+   
+       // Preparar los valores para la actualización
+       const values = [
+         fecha_inicio_sub,
+         fecha_fin_sub,
+         imagen_sub,
+         precio_inicial_sub,
+         precio_final_sub,
+         unidad_peso_sub,
+         cantidad_sub,
+         estado_sub,
+         certificado_sub,
+         descripcion_sub,
+         fk_variedad,
+         id, // El ID de la subasta a actualizar
+       ];
+   
+       const [resultado] = await pool.query(
+         "UPDATE subasta SET fecha_inicio_sub=COALESCE(?, fecha_inicio_sub), fecha_fin_sub=COALESCE(?, fecha_fin_sub), imagen_sub=COALESCE(?, imagen_sub), precio_inicial_sub=COALESCE(?, precio_inicial_sub), precio_final_sub=COALESCE(?, precio_final_sub), unidad_peso_sub=COALESCE(?, unidad_peso_sub), cantidad_sub=COALESCE(?, cantidad_sub), estado_sub=COALESCE(?, estado_sub), certificado_sub=COALESCE(?, certificado_sub), descripcion_sub=COALESCE(?, descripcion_sub), fk_variedad=COALESCE(?, fk_variedad) WHERE pk_id_sub=?",
+         values
+       );
+   
+       if (resultado.affectedRows > 0) {
+         res.status(200).json({
+           mensaje: "La subasta ha sido actualizada exitosamente",
+         });
+       } else {
+         res.status(404).json({
+           mensaje: "No se encontró ninguna subasta con el id proporcionado",
+         });
+       }
     } catch (error) {
-        console.error("Error en el bloque try:", error);
-        res.status(500).json({
-            "mensaje": "Error interno del servidor"
-        });
+       console.error("Error en el bloque try:", error);
+       res.status(500).json({
+         mensaje: "Error interno del servidor",
+       });
     }
-}
-
+   };
+   
+// busca una subasta
 export const buscar = async (req, res) => {
-    try {
-        const subastaId = req.params.id;
+  try {
+    const subastaId = req.params.id;
 
-        if (!subastaId) {
-            return res.status(400).json({
-                "mensaje": "Por favor, proporcione un id de subasta válido."
-            });
-        }
-
-        const [resultado] = await pool.query("select * from subasta where pk_id_sub = ?", [subastaId]);
-
-        if (resultado.length > 0) {
-            res.status(200).json({
-                "mensaje": "Datos de subasta obtenidos correctamente",
-                "data": resultado[0]
-            });
-        } else {
-            res.status(404).json({
-                "mensaje": "No se encontró ninguna subasta con el id consultado."
-            });
-        }
-
-    } catch (error) {
-        console.error("Error en el bloque try:", error);
-        res.status(500).json({
-            "mensaje": "Error interno del servidor"
-        });
+    if (!subastaId) {
+      return res.status(400).json({
+        mensaje: "Por favor, proporcione un id de subasta válido.",
+      });
     }
-}
 
-export const eliminar = async (req, res) => {
-    try {
-        const id = req.params.id;
+    const [resultado] = await pool.query(
+      "select * from subasta where pk_id_sub = ?",
+      [subastaId]
+    );
 
-        const [resultado] = await pool.query("delete from subasta where pk_id_sub = ?", [id]);
-
-        if (resultado.affectedRows > 0) {
-            res.status(200).json({
-                "mensaje": "Subasta eliminada exitosamente."
-            });
-        } else {
-            res.status(404).json({
-                "mensaje": "No se encontró una subasta con el id proporcionado."
-            });
-        }
-
-    } catch (error) {
-        console.error("Error en el bloque try:", error);
-        res.status(500).json({
-            "mensaje": "Error interno del servidor"
-        });
+    if (resultado.length > 0) {
+      res.status(200).json({
+        mensaje: "Datos de subasta obtenidos correctamente",
+        data: resultado[0],
+      });
+    } else {
+      res.status(404).json({
+        mensaje: "No se encontró ninguna subasta con el id consultado.",
+      });
     }
+  } catch (error) {
+    console.error("Error en el bloque try:", error);
+    res.status(500).json({
+      mensaje: "Error interno del servidor",
+    });
+  }
 };
+// elimina una subasta
+export const eliminar = async (req, res) => {
+  try {
+    const id = req.params.id;
 
+    const [resultado] = await pool.query(
+      "delete from subasta where pk_id_sub = ?",
+      [id]
+    );
 
+    if (resultado.affectedRows > 0) {
+      res.status(200).json({
+        mensaje: "Subasta eliminada exitosamente.",
+      });
+    } else {
+      res.status(404).json({
+        mensaje: "No se encontró una subasta con el id proporcionado.",
+      });
+    }
+  } catch (error) {
+    console.error("Error en el bloque try:", error);
+    res.status(500).json({
+      mensaje: "Error interno del servidor",
+    });
+  }
+};
--- backend/src/routes/subasta.routes.js
+++ backend/src/routes/subasta.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { listar, registrar, actualizar, buscar, eliminar } from "../controllers/subasta.controllers.js";
+import { subastaFiles, listar, registrar, actualizar, buscar, eliminar } from "../controllers/subasta.controllers.js";
 import { validarActualizarSubasta, validarRegistrarSubasta} from "../validations/subasta.validation.js";
 import { verificarUserToken } from "../controllers/autenticacionController.js";
 
 const router = Router()
 
+router.post("/registrar",subastaFiles,verificarUserToken, validarRegistrarSubasta, registrar)
 router.get("/listar",verificarUserToken, listar)
-router.post("/registrar",verificarUserToken, validarRegistrarSubasta, registrar)
-router.put("/actualizar/:id",verificarUserToken, validarActualizarSubasta, actualizar)
+router.put("/actualizar/:id",subastaFiles,verificarUserToken, validarActualizarSubasta, actualizar)
 router.get("/buscar/:id",verificarUserToken, buscar)
 router.delete("/eliminar/:id",verificarUserToken, eliminar)
 
