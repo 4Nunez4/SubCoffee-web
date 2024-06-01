c32c16c680dd52ccd6a7e6202c191cd861fbd758
4Nune4 <jorgeenrrique123456789@gmail.com>
2024-06-01 11:15:47 -0500

    practicas back

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
-import rutNotificaciones from "./src/routes/notificaciones.routes.js";
-import routerChat from "./src/routes/chat.routes.js";
-import rutasSubastas from "./src/routes/subasta.routes.js";
-import postulantesRoutes from "./src/routes/postulantes.routes.js";
-import autenticacionRouter from "./src/routes/autenticacion.routes.js";
 import cors from 'cors';
-
-import routerUser from "./src/routes/user.routes.js";
-import routerVereda from "./src/routes/veredas.routes.js";
-import routerDepart from "./src/routes/departamento.routes.js"
-import routerMunicipio from "./src/routes/municipio.routes.js"
-import routerFinca from "./src/routes/finca.routes.js"
-import routerVariedad from "./src/routes/variedad.routes.js"
-import routertipovari from "./src/routes/tipovariedad.routes.js"
-import ofertasRoutes from "./src/routes/ofertas.routes.js";
-import routesCalificaciones from "./src/routes/calificaciones.routes.js";
 
 const app = express();
 app.use(cors());
-const PORT = 4000
+const PORT = 
+
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/auth", autenticacionRouter)
-app.use("/v1", routerUser);
-app.use("/v1", routerDepart);
-app.use("/v1", routerVereda);
-app.use("/v1", routerMunicipio);
-app.use("/v1", routerFinca);
-app.use("/v1", routertipovari);
-app.use("/v1", routerVariedad);
-app.use("/v1", rutasSubastas);
-app.use("/user", routerChat);
-app.use("/v1", rutNotificaciones);
-app.use("/v1", postulantesRoutes);
-app.use("/v1", ofertasRoutes);
-app.use("/v1", routesCalificaciones);
 
 app.set("view engine", "ejs");
 
