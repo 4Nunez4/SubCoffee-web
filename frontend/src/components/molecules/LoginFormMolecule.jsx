ccdedfe6c7450bc027833ed9e077560909f66ccf
4Nune4 <jorgeenrrique123456789@gmail.com>
2024-06-05 04:19:09 -0500

    casi completo SubCoffee falta ajustar cosas pequeñas

--- frontend/src/components/molecules/LoginFormMolecule.jsx
+++ frontend/src/components/molecules/LoginFormMolecule.jsx
@@ -81,13 +81,8 @@ const LoginFormMolecule = () => {
       >
         ¿Olvidaste tu contraseña?
       </a>
-<<<<<<< HEAD
-      <ModalFooter className="flex justify-center ">
-        <button type="submit"   className="inline-flex items-center justify-center py-2 px-4 bg-[#001e2b] text-white font-semibold rounded-md hover:bg-[#00ed64] border-2 hover:border-[#00ed64] hover:text-[#001e2b] transition-all ease-in-out duration-500"> 
-=======
       <ModalFooter className="flex justify-center">
         <Button type="submit" className="inline-flex items-center justify-center py-2 px-4 bg-[#001e2b] text-white font-semibold rounded-md hover:bg-[#00ed64] border-2 hover:border-[#00ed64] hover:text-[#001e2b] transition-all ease-in-out duration-500"> 
->>>>>>> DEVjcrc
           Iniciar Sesión
         </Button>
       </ModalFooter>
