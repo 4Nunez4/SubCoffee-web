d6c2d563a4ec12e20f97d93b46cb64f010b4385a
4Nune4 <jorgeenrrique123456789@gmail.com>
2024-06-12 22:31:35 -0500

    estilos de slider imagen de la pagina de inicio

--- frontend/src/components/molecules/ImageSlider.jsx
+++ frontend/src/components/molecules/ImageSlider.jsx
@@ -33,36 +33,48 @@ function ImageSlider() {
 
   return (
     <div>
-      <div className="h-auto w-full m-auto pt-10 p-4">
-        <div className="relative lg:w-full h-[450px] rounded-2xl overflow-hidden">
-          <div
-            className="w-full h-full bg-center bg-cover duration-500"
-            style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          ></div>
-        <div className="absolute inset-0 flex items-center justify-center text-center bg-[#061621] bg-opacity-60 p-8">
-          <div>
-            <h2 className="text-4xl font-bold text-white mb-4">¡Bienvenido a Subcoffee!</h2>
-            <p className="text-2xl text-white font-semibold leading-relaxed">
-              Descubre la mejor plataforma online para subastar por cafés especiales. ¡No pierdas la oportunidad de obtener un café de alta calidad!
-            </p>
+    <div className="max-w-[1600px] h-[350px]  w-full m-auto pt-10 p-4 bg-[#FDFBF6]">
+      <div className="relative w-full h-[270px] rounded-lg overflow-hidden">
+        <div
+          className="w-full h-4/5 bg-center bg-cover duration-500 rounded-lg"
+          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        ></div>
+      <div className="absolute inset-0 flex items-center h-4/5 justify-center text-center bg-[#181818] bg-opacity-70  rounded-lg">
+
+      <div className=" flex flex-col items-center justify-center">
+            <div className="bg-[#82828286] h-5/6 w-4/5 rounded-lg flex justify-center items-center text-lg font-medium ">
+              <div className=" w-3/4 h-auto ">
+                <div className="text-center text-[#FDFBF6] ">
+                  <h1 className="text-5xl font-bold mb-4">
+                    Bienvenido a Sub
+                    <span className="text-[#39A800]">Coffee</span>
+                  </h1>
+                  <p className=" py-4">
+                    Una plataforma online donde te podrás conectar con
+                    diferentes usuarios para subastar y pujar por café de alta
+                    calidad. Empieza a subastar ya
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        </div>
-        <div className="flex justify-center mt-4 ">
-          <div className="flex justify-center items-center transition duration-300 ">
-            {slides.map((slide, index) => (
-              <div
-                key={index}
-                className={`w-3 h-3 mx-2 rounded-full cursor-pointer ${
-                  index === currentIndex ? "bg-[#00684a]" : "bg-gray-300"
-                }`}
-                onClick={() => setCurrentIndex(index)}
-              ></div>
-            ))}
-          </div>
+      </div>
+      </div>
+      <div className="">
+        <div className="flex justify-center items-center transition duration-300 ">
+          {slides.map((slide, index) => (
+            <div
+              key={index}
+              className={`w-2 h-2 mx-2 rounded-lg cursor-pointer ${
+                index === currentIndex ? "bg-[#39A800]" : "bg-[#A6D290]"
+              }`}
+              onClick={() => setCurrentIndex(index)}
+            ></div>
+          ))}
         </div>
       </div>
     </div>
+  </div>
   );
 }
 
